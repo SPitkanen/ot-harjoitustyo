b6a9410aa4af3685d86f744811e8a7e47740ec2f
SPitkanen <77848087+SPitkanen@users.noreply.github.com>
2021-05-04 17:46:41 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, password TEXT, role TEXT, visible INTEGER);
-CREATE TABLE aircraft (id SERIAL PRIMARY KEY, type TEXT, register, TEXT);
+CREATE TABLE aircraft (id SERIAL PRIMARY KEY, type TEXT, register TEXT);
+CREATE TABLE users (id SERIAL PRIMARY KEY, name TEXT, password TEXT, role INTEGER, visible INTEGER);
 CREATE TABLE acData (id SERIAL PRIMARY KEY, acid INTEGER REFERENCES aircraft (id), item TEXT, arm DECIMAL, weight DECIMAL, moment DECIMAL, maxweight DECIMAL);
-CREATE TABLE acDataDepend (id SERIAL PRIMARY KEY, acid INTEGER REFERENCES aircraft (id), item TEXT, arm DECIMAL, weight DECIMAL, moment DECIMAL, maxweight DECIMAL, section INTEGER, operation INTEGER);
-CREATE TABLE wbtemplate (id SERIAL PRIMARY KEY, planeid INTEGER REFERENCES aircraft (id), xstart DECIMAL, xstop DECIMAL, ystart DECIMAL, ystop DECIMAL, xspacing DECIMAL, yspacing DECIMAL);
-CREATE TABLE wbenvelope (id SERIAL PRIMARY KEY, planeid INTEGER REFERENCES aircraft (id), x DECIMAL, y DECIMAL);
+CREATE TABLE acDataDepend (id SERIAL PRIMARY KEY, acid INTEGER REFERENCES aircraft (id), item TEXT, arm DECIMAL, weight DECIMAL, moment DECIMAL, maxweight DECIMAL, section INTEGER, operation INTEGER); 
+CREATE TABLE wbtemplate (id SERIAL PRIMARY KEY, planeId INTEGER REFERENCES aircraft(id), xstart DECIMAL, xstop DECIMAL, ystart DECIMAL, ystop DECIMAL, xspacing DECIMAL, yspacing DECIMAL);
+CREATE TABLE wbenvelope (id SERIAL PRIMARY KEY, planeId INTEGER REFERENCES aircraft(id), x DECIMAL, y DECIMAL);
+CREATE TABLE results (id SERIAL PRIMARY KEY, acid INTEGER REFERENCES aircraft (id), userid INTEGER REFERENCES users (id), item TEXT, arm DECIMAL, weight DECIMAL, moment DECIMAL, visible INTEGER, date TEXT);
 
